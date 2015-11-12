3e4aafa0c7d4ec0c35207b4a7d5a139655044f43
artsoroka <githubrepository@yandex.ru>
2015-11-12 21:21:28 +0300

    moved db connection params to config

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	App: {
 		port: process.env.PLAYLIST_PORT || 8080
+	}, 
+	db: {
+	    user     : process.env.PL_DB_USER || 'playlistadmin', 
+	    password : process.env.PL_DB_PSWD, 
+	    database : process.env.PL_DB_NAME || 'playlist', 
+	    port	 : process.env.PL_DB_PORT || 3306
 	}
 }
